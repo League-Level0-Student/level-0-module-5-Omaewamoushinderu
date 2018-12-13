5e4fde0f2a1be080810779e23cb18991b35f2c05
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-13 13:23:15 -0800

    CircleRing, ForLoopGauntlet(IP)

--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -7,29 +7,39 @@ package nested_loops;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
 
+		Robot r = new Robot();
+		// 2. Set your robot’s position to x=150 and y=200
+		r.setX(150);
+		r.setY(200);
+		// 3. Put the robot's pen down
+		r.penDown();
+		// 4. Set the robot’s speed to 10
+		r.setSpeed(10);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
+			r.move(3);
+			r.turn(1);
+			if (i % 20 == 0) {
+				for(int j=0 ; j<360 ; j++) {
+				r.move(1);
+				r.turn(1);
+				}
+			}
+		}
+		// 6. Move the robot 3 pixels
 
-       //  2. Set your robot’s position to x=150 and y=200
+		// 7. Turn the robot 1 degree
 
-        // 3. Put the robot's pen down
-
-        // 4. Set the robot’s speed to 10
-
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
+		// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
 
 		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
 
-				// 10. Move the robot 1 pixel
+		// 10. Move the robot 1 pixel
 
-				// 11. Turn the robot 1 degree
-}
+		// 11. Turn the robot 1 degree
+	}
 }
