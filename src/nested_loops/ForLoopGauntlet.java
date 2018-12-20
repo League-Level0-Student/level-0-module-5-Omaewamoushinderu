637b7cdde8d7569af0a2f75eba7abc144b5922e7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-20 13:25:02 -0800

    ForLoopGauntlet(IP)

---
+++ src/nested_loops/ForLoopGauntlet.java
@@ -0,0 +1,31 @@
+package nested_loops;
+
+public class ForLoopGauntlet {
+public static void main(String[] args) {
+	//for(int i=0 ; i<100 ; i++) {
+	//	System.out.println(i);
+	//}
+	//for(int i=100 ; i>=0 ; i--) {
+		//System.out.println(i + " Jingle balls");
+	//}
+	//for(int i=2 ; i<102 ; i++) {
+		//if(i%2==0) {
+			//System.out.println(i + " christmas cheers");
+		//}
+	//}
+	//for(int i=1 ; i<100 ; i++ ) {
+		//if(i%2==1) {
+			//System.out.println(i);
+		//}
+	//}
+
+for(int i=0; i<501 ; i++) {
+if(i%2==1) {
+	System.out.println(i + " is odd");
+}
+else if(i%2==0) {
+	System.out.println(i + " is even");
+}
+}
+}
+}
