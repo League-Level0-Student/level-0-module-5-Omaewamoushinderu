6f5a80d651da516b7fd18af010288567564c2a26
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-14 13:19:52 -0800

    ForLoopGauntlet(IP)

--- src/nested_loops/ForLoopGauntlet.java
+++ src/nested_loops/ForLoopGauntlet.java
@@ -1,57 +1,66 @@
 package nested_loops;
 
 public class ForLoopGauntlet {
-public static void main(String[] args) {
-	for(int i=0 ; i<3 ; i++) {
-		for(int a=0 ; a<3 ; a++) {
-			System.out.println(i + " " + a + " " + i + " " );
-		}
+	public static void main(String[] args) {
+for(int i=1 ; i<6 ; i+=6) {
+	for(int o=0 ; o<4; o++) {
+		System.out.print( i + "*");
 	}
-	//for(int i=0; i<3 ; i++) {
-		
-		//for(int a=0 ; a<3 ; a++) {
-			//System.out.println(i + " " + a);
-			//}
-		//}
-		
-	//}
-	//int years = 0;
-	//for(int i=2006; i<2020; i++) {
-		
-	
-		//System.out.println("in " + i + " i was " + years++ );
-	//}
-	//for(int i=7 ; i<778; i++) {
-		//if(i%7==0) {
-			//System.out.println(i);
-		//}
-		
-	//}
-	//for(int i=0 ; i<100 ; i++) {
-	//	System.out.println(i);
-	//}
-	//for(int i=100 ; i>=0 ; i--) {
-		//System.out.println(i + " Jingle balls");
-	//}
-	//for(int i=2 ; i<102 ; i++) {
-		//if(i%2==0) {
-			//System.out.println(i + " christmas cheers");
-		//}
-	//}
-	//for(int i=1 ; i<100 ; i++ ) {
-		//if(i%2==1) {
-			//System.out.println(i);
-		//}
-	//}
-
-//for(int i=0; i<501 ; i++) {
-//if(i%2==1) {
-	//System.out.println(i + " is odd");
-//}
-//else if(i%2==0) {
-	//System.out.println(i + " is even");
-	
+	System.out.println();
 }
+		/*
+		 * for(int i=1 ; i<100 ; i+=10) { for(int a=0 ; a<10 ; a++) {
+		 * System.out.print(i+a);
+		 * 
+		 * } System.out.println(); }
+		 */
+		/*
+		 * for(int i=0 ; i<9 ; i+=3) {
+		 * 
+		 * for(int a=1 ; a<4 ; a++) { System.out.print(a + i); } System.out.println(); }
+		 */
+		// for(int i=0; i<3 ; i++) {
+
+		// for(int a=0 ; a<3 ; a++) {
+		// System.out.println(i + " " + a);
+		// }
+		// }
+
+		// }
+		// int years = 0;
+		// for(int i=2006; i<2020; i++) {
+
+		// System.out.println("in " + i + " i was " + years++ );
+		// }
+		// for(int i=7 ; i<778; i++) {
+		// if(i%7==0) {
+		// System.out.println(i);
+		// }
+
+		// }
+		// for(int i=0 ; i<100 ; i++) {
+		// System.out.println(i);
+		// }
+		// for(int i=100 ; i>=0 ; i--) {
+		// System.out.println(i + " Jingle balls");
+		// }
+		// for(int i=2 ; i<102 ; i++) {
+		// if(i%2==0) {
+		// System.out.println(i + " christmas cheers");
+		// }
+		// }
+		// for(int i=1 ; i<100 ; i++ ) {
+		// if(i%2==1) {
+		// System.out.println(i);
+		// }
+		// }
+
+		// for(int i=0; i<501 ; i++) {
+		// if(i%2==1) {
+		// System.out.println(i + " is odd");
+		// }
+		// else if(i%2==0) {
+		// System.out.println(i + " is even");
+
+	}
 }
-
-
